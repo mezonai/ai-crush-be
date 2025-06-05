9c23762c5b57cc9c827cf864e3f585d7ebd0d384
trinhlenhat <trinh.lenhat@ncc.asia>
2025-06-05 15:29:21 +0700

    feat: add swagger for documentation

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const configSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('AI-Crush')
+    .setDescription("AI-Crush's API definition")
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document, {
+    swaggerOptions: { persistAuthorizationL: true },
+    customCss: '.swagger-ui .topbar { display: none }',
+  });
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import { configSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,10 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+  app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+  configSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
