410b5145bb4a858133eeb9e1b39f25477a2c2b8e
trinhlenhat <trinh.lenhat@ncc.asia>
2025-06-05 14:16:16 +0700

    feat: add exception handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserController } from './features/user/user.controller';
-import { UserService } from './features/user/user.service';
 import { envFilePath } from './config/env-path.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOption } from './config/data-source.config';
+import { UserModule } from './features/user/user.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 @Module({
   imports: [
@@ -13,8 +14,14 @@ import { dataSourceOption } from './config/data-source.config';
       envFilePath: envFilePath
     }),
     TypeOrmModule.forRoot(dataSourceOption),
+    UserModule
   ],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule { }
